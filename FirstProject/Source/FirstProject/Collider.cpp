d0bd79b3cd95b92f333bae86ad093f0b9ef58259
jinabro <kmy4869@naver.com>
2022-02-20 21:55:26 +0900

    ~29

---
+++ FirstProject/Source/FirstProject/Collider.cpp
@@ -0,0 +1,93 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Collider.h"
+#include "Components/SphereComponent.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "ColliderMovementComponent.h"
+// Sets default values
+ACollider::ACollider()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+	Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	SetRootComponent(Sphere);
+	Sphere->InitSphereRadius(40.f);
+	Sphere->SetCollisionProfileName(TEXT("Pawn"));
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	Mesh->SetupAttachment(GetRootComponent());
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_Collider(TEXT("StaticMesh'/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere'"));
+	if (SM_Collider.Succeeded())
+	{
+		Mesh->SetStaticMesh(SM_Collider.Object);
+		Mesh->SetRelativeLocation(FVector(0.f, 0.f, -40.f));
+		Mesh->SetWorldScale3D(FVector(0.8f, 0.8f, 0.8f));
+	}
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
+	SpringArm->TargetArmLength = 400.f;
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->CameraLagSpeed = 3.f;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+
+	OurMovementComponent = CreateDefaultSubobject<UColliderMovementComponent>(TEXT("OurMovementComponent"));
+	OurMovementComponent->UpdatedComponent = GetRootComponent();
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+}
+
+// Called when the game starts or when spawned
+void ACollider::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACollider::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACollider::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ACollider::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &ACollider::MoveRight);
+}
+
+UPawnMovementComponent* ACollider::GetMovementComponent() const
+{
+	return OurMovementComponent;
+}
+
+void ACollider::MoveForward(float Value)
+{
+	FVector Forward = GetActorForwardVector();
+	if (OurMovementComponent)
+	{
+		OurMovementComponent->AddInputVector(Forward * Value);
+	}
+}
+
+void ACollider::MoveRight(float Value)
+{
+	FVector Right = GetActorRightVector();
+	if (OurMovementComponent)
+	{
+		OurMovementComponent->AddInputVector(Right * Value);
+	}
+}
+
---
+++ FirstProject/Source/FirstProject/Collider.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Collider.generated.h"
+
+UCLASS()
+class FIRSTPROJECT_API ACollider : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACollider();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	class UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	class USphereComponent* Sphere;
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	class UCameraComponent* Camera;
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	class USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, Category = "Movement")
+	class UColliderMovementComponent* OurMovementComponent;
+
+	virtual UPawnMovementComponent* GetMovementComponent() const override;
+
+public:
+	FORCEINLINE UStaticMeshComponent* GetMeshComponent() { return Mesh; }
+	FORCEINLINE void SetMeshComponent(UStaticMeshComponent* MeshComponent) { Mesh = MeshComponent; }
+
+	FORCEINLINE USphereComponent* GetSphereComponent() { return Sphere; }
+	FORCEINLINE void SetSphereComponent(USphereComponent* SphereComponent) { Sphere = SphereComponent; }
+
+	FORCEINLINE UCameraComponent* GetCameraComponent() { return Camera; }
+	FORCEINLINE void SetCameraComponent(UCameraComponent* CameraComponent) { Camera = CameraComponent; }
+
+	FORCEINLINE USpringArmComponent* GetSpringArmComponent() { return SpringArm; }
+	FORCEINLINE void SetSpringArmComponent(USpringArmComponent* SpringArmComponent) { SpringArm = SpringArmComponent; }
+
+
+
+private:
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
+};
