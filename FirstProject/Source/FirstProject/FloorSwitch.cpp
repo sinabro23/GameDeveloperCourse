64a8efc5775b502721a9a87b6b81bff7f5eb0e25
jinabro <kmy4869@naver.com>
2022-02-21 23:23:40 +0900

    ~42

---
+++ FirstProject/Source/FirstProject/FloorSwitch.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FloorSwitch.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AFloorSwitch::AFloorSwitch()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger"));
+	RootComponent = TriggerBox;
+
+	TriggerBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	TriggerBox->SetCollisionObjectType(ECollisionChannel::ECC_WorldStatic);
+	TriggerBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	TriggerBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	TriggerBox->SetBoxExtent(FVector(62.f, 62.f, 32.f));
+	TriggerBox->SetGenerateOverlapEvents(true);
+
+	FloorSwitch = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FloorSitch"));
+	FloorSwitch->SetupAttachment(GetRootComponent());
+
+	DoorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorMesh"));
+	DoorMesh->SetupAttachment(GetRootComponent());
+
+
+}
+
+// Called when the game starts or when spawned
+void AFloorSwitch::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &AFloorSwitch::OnOverlapBegin);
+	TriggerBox->OnComponentEndOverlap.AddDynamic(this, &AFloorSwitch::OnOverlapEnd);
+}
+
+// Called every frame
+void AFloorSwitch::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AFloorSwitch::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Overlap Begin"));
+}
+
+void AFloorSwitch::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Overlap End"));
+}
+
